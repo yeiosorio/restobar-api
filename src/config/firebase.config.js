c0233ca9ada1933eae778247e26e7b6e3c0eb469
yeiosorio <yei_osorio@hotmail.com>
2025-01-15 12:25:04 -0500

    refactor: actualiza firebase config para usar variables de entorno de credenciales v3

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -8,7 +8,7 @@ admin.initializeApp({
     private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL
   }),
-  storageBucket: 'restobar-33413.firebasestorage.app' 
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
 });
 
 const bucket = admin.storage().bucket();
